c18c04dc50107acf9d8835c315d3a658901937f6
Vitória <vitoriafelixpe@gmail.com>
2025-07-10 14:45:43 -0300

    Update script.js

--- PROJETO/script.js
+++ PROJETO/script.js
@@ -10,7 +10,6 @@ const markdownToHTML = (text) => {
     return converter.makeHtml(text)
 }
 
-//AIzaSyAxm45DEaDcfjEHM4K2U0RE6Cbj796OnaI
 const askAI = async(question, game, apiKey) => {
     const model = "gemini-2.5-flash"
     const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}` 
